d97e1b2bd386faef7e31e012335f79a7123ddceb
Morgan Thomas <morganrhysthoams14@gmail.com>
2023-01-29 00:14:24 +0000

    Fixed environment variable names

--- web/config.py
+++ web/config.py
@@ -4,9 +4,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQL_USER = os.environ.get("DB_USER")
-    SQL_PASSWORD = os.environ.get("DB_PASSWORD")
-    SQL_HOST = os.environ.get("DB_HOST")
+    SQL_USER = os.environ.get("POSTGRES_USER")
+    SQL_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
+    SQL_HOST = os.environ.get("POSTGRES_HOST")
     KAFKA = os.environ.get("KAFKA")
     MONGO_HOST =os.environ.get("MONGO_HOST")
     MONGO_USER = os.environ.get("MONGO_USER")
