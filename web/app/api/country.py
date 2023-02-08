5151c4df98c1790dd7bd3ca0ee42d268c08fa9b0
Morgan Thomas <morganrhysthoams14@gmail.com>
2023-02-08 21:25:01 +0000

    Added timings field

--- web/app/api/country.py
+++ web/app/api/country.py
@@ -32,6 +32,7 @@ def get_overview(current_app: current_app):
     bottom_5_towns = current_app.mongo_db.cache.aggregate(query)
     country_data = current_app.mongo_db.cache.find_one({"_id": "ALLCOUNTRY"})
     return_data = country_data["stats"]
+    return_data["timings"] = country_data["timings"]
     return_data["top_five"] = list(top_5_towns)
     return_data["bottom_five"] = list(bottom_5_towns)
     return return_data
