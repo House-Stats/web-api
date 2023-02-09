33eb1f6783a7d2923d72536ef89020c696cb1d4b
Morgan Thomas <morganrhysthoams14@gmail.com>
2023-02-09 11:53:38 +0000

    Added sentry to celery

--- web/app/celery/__init__.py
+++ web/app/celery/__init__.py
@@ -1,7 +1,9 @@
 import os
 
-from celery import Celery
+from celery import Celery, signals
 from app.celery.analyse import Analyse
+import sentry_sdk
+from sentry_sdk.integrations.celery import CeleryIntegration
 
 # Initialize Celery
 celery = Celery(
@@ -9,6 +11,17 @@ celery = Celery(
     broker= os.environ.get("CELERY_BROKER_URL"),
     backend=os.environ.get("CELERY_RESULT_BACKEND")
 )
+
+@signals.celeryd_init.connect
+def init_sentry(**_kwargs):
+    if not os.environ["DEBUG"]:
+        sentry_sdk.init(
+            dsn="https://925f26cce4d34132bb9fcc5e38db1eed@o4504585220980736.ingest.sentry.io/4504649955278848",
+            traces_sample_rate=1.0,
+            integrations=[
+                CeleryIntegration(),
+            ]
+        )
 
 @celery.task()
 def analyse_task(area: str, area_type: str):
