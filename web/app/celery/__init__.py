abbdd8764dc9381591ae11b175f5523b9c65d3ab
Morgan Thomas <morganrhysthoams14@gmail.com>
2023-02-13 11:53:11 +0000

    Added valuation worker

--- web/app/celery/__init__.py
+++ web/app/celery/__init__.py
@@ -1,9 +1,13 @@
 import os
 
-from celery import Celery, signals
+from celery import Celery, signals, group
+from celery.result import allow_join_result
 from app.celery.analyse import Analyse
 import sentry_sdk
 from sentry_sdk.integrations.celery import CeleryIntegration
+from app.celery.valuation import Valuation
+import time
+from typing import List, Tuple
 
 # Initialize Celery
 celery = Celery(
@@ -29,5 +33,30 @@ def analyse_task(area: str, area_type: str):
     area_type = area_type.upper()
     aggregator = Analyse()
     aggregator.run(area, area_type)
-    del aggregator
+    aggregator.clean_up()
     return area + area_type
+
+@celery.task()
+def valuation_task(houseid: str):
+    valuater = Valuation()
+    if valuater.check_house(houseid):
+        areas = valuater.get_areas()
+        get_analysis_of_areas(areas)
+        aggs = valuater.load_aggregations(areas)
+        perc_changes = valuater.find_monthly_averages(aggs)
+        sales = valuater.get_house_sales()
+        valuations = valuater.calc_latest_price(sales, perc_changes)
+        return valuations
+    else:
+        return "No House Found"
+
+def get_analysis_of_areas(areas: List[Tuple[str,str]]) -> None:
+    tasks = []
+    for area in areas:
+        tasks.append(analyse_task.subtask(area))
+    tasks = group(tasks)
+    job = tasks.apply_async()
+    while not job.ready():
+        time.sleep(0.2)
+    with allow_join_result():
+        results = job.get()
